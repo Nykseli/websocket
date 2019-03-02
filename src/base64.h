3e946b2845d140b16bb654a37b92749582b9b290
Miika Alikirri <miika.alik@gmail.com>
2019-03-02 17:23:12 +0200

    Forgot to remove the base64 from src/

--- src/base64.h
+++
@@ -1,7 +0,0 @@
-#ifndef WEB_SOCKET_BASE64_H
-#define WEB_SOCKET_BASE64_H
-
-int base64encode(const char *input, char *output, int len);
-int base64decode(const char *input, char *output, int len);
-
-#endif
